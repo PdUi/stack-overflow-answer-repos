25ffdb4edb0a82e3f68cd6d6af9f7714350bcb85
PeinearyDevelopment <peinearydevelopment@gmail.com>
2019-02-22 12:23:05 -0500

    added css doodle example

---
+++ ng-css-doodle/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import 'css-doodle';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'ng-css-doodle';
+}
